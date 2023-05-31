337e9ee1554f67ce54a52b5b5a1530abeb7cecf3
Poonam sengar <sengarpoonam2699@gmail.com>
2023-05-31 19:17:16 +0530

    main

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,18 @@
 let movieDetails = null;
 let from = 0;
-let to =4;
+let to = 4;
 function fetchMovies(key, elementId) {
-   fetch(`http://www.omdbapi.com/?s=${key}&apikey=7ac316df`)
+  fetch(`http://www.omdbapi.com/?s=${key}&apikey=7ac316df`)
     .then((response) => response.json())
     .then((data) => {
       movieDetails = data.Search;
       createMovieCard(elementId);
-      
     });
 }
 function createMovieCard(elementId) {
-
   let cards = "";
   let docCard = document.getElementById(elementId);
- 
+
   for (let i = from; i <= to; ++i) {
     let movie = movieDetails[i];
     cards += `<div>
@@ -25,17 +23,17 @@ function createMovieCard(elementId) {
     <p>${movie.Type}</p>
   </div> `;
   }
-  cards = 
-  // `<div class="slideBtn"><i onclick="setSlideData('left','cardId')" class="fa-solid fa-angle-left"></div>`
-   cards +
- `<div class="slideBtn"><i onclick="setSlideData('right','cardId')" class="fa-solid fa-angle-right"></div>`;
+  cards =
+    `<div class="sliderParent"><div  onclick="setSlideData('left','${elementId}')" class="slideBtn"><</div></div>` +
+    cards +
+    `<div class="sliderParent"><div onclick="setSlideData('right','${elementId}')" class="slideBtn">></div></div>`;
 
- docCard.innerHTML = cards;
+  docCard.innerHTML = cards;
 }
 
 fetchMovies("Tom", "cardId1");
 fetchMovies("jerry", "cardId2");
-fetchMovies("doremon", "cardId3");
+// fetchMovies("dora", "cardId3");
 fetchMovies("spider-man", "cardId4");
 fetchMovies("jerry", "cardId5");
 fetchMovies("nobita", "cardId6");
@@ -48,25 +46,24 @@ fetchMovies("sunrise", "cardId12");
 fetchMovies("Tom", "cardId13");
 fetchMovies("jerry", "cardId14");
 fetchMovies("kids", "cardId15");
-fetchMovies("chhota bheem","premireCard");
+fetchMovies("chhota bheem", "premireCard");
 
-function setSlideData(slideDirection, elementId){
-  if(slideDirection == "left"){
+function setSlideData(slideDirection, elementId) {
+  console.log(from, to, elementId);
+  if (slideDirection == "left") {
     to -= 4;
     from -= 4;
-  }else{
+  } else {
     to += 4;
     from += 4;
   }
-  if(from < 0){
+  if (from < 0) {
     from = 0;
-    to = 4
+    to = 4;
   }
-  if(to > movieDetails.length){
+  if (to > movieDetails.length) {
     from = movieDetails.length - 5;
     to = movieDetails.length - 1;
   }
-
   createMovieCard(elementId);
 }
-
